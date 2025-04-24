22da267005f982e171b9740eddebd98f7a05a875
Leander Hughes <ST01471529@vcconnect.edu.za>
2025-04-24 15:35:54 +0200

    initial: forth commit,code redu

--- app/src/main/java/com/example/gameofquizz/MainActivity2.kt
+++ app/src/main/java/com/example/gameofquizz/MainActivity2.kt
@@ -18,19 +18,7 @@ class MainActivity2 : AppCompatActivity() {
     private lateinit var nextquestionBtn: Button
     private lateinit var resultsBtn: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
-
-        questionDisplay = findViewById(R.id.questionDisplay)
-        answerDisplay = findViewById(R.id.answerDisplay)
-        trueBtn = findViewById(R.id.trueBtn)
-        falseBtn = findViewById(R.id.falseBtn)
-        nextquestionBtn = findViewById(R.id.nextQuestionBtn)
-        resultsBtn = findViewById(R.id.resultsBtn)
-
-        val questions = arrayOf(
+        private val questions = arrayOf(
             "The Earth is round?",
             "In a deck of cards,the king has a moustache?",
             "Volkswagen own's BMW?",
@@ -42,7 +30,7 @@ class MainActivity2 : AppCompatActivity() {
             "An octopus has three hearts?",
             "The Eiffel Tower is located in Paris?",
         )
-        val answers = arrayOf(
+        private val answers = arrayOf(
             "True",
             "False",
             "False",
@@ -55,44 +43,71 @@ class MainActivity2 : AppCompatActivity() {
             "True",
         )
         // This line of code is simply keeps track of how many questions you have gone through
-        var currentQuestionIndex = 0
+       private var currentQuestionIndex = 0
         // This line of code simply keeps track ot the score
-        var score = 0
+       private var score = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main2)
+
+        questionDisplay = findViewById(R.id.questionDisplay)
+        answerDisplay = findViewById(R.id.answerDisplay)
+        trueBtn = findViewById(R.id.trueBtn)
+        falseBtn = findViewById(R.id.falseBtn)
+        nextquestionBtn = findViewById(R.id.nextQuestionBtn)
+        resultsBtn = findViewById(R.id.resultsBtn)
+
+        displayQuestion()
 
         // This line of code is where i've set the first question and answer to be displayed
         questionDisplay.text = questions[currentQuestionIndex]
         answerDisplay.text = ""
 
         trueBtn.setOnClickListener {
-            if (answers[currentQuestionIndex] == "True") {
-                score++
-            }
+            checkAnswer("True")
         }
         falseBtn.setOnClickListener {
-            if (answers[currentQuestionIndex] == "False") {
-                score++
-            }
+            checkAnswer("False")
         }
         nextquestionBtn.setOnClickListener {
-            currentQuestionIndex++
-            if (currentQuestionIndex < questions.size) {
-                questionDisplay.text = questions[currentQuestionIndex]
-                answerDisplay.text = ""
-                trueBtn.isEnabled = true
-                falseBtn.isEnabled = true
-            } else {
-                // All questions have been answered, show results
-                val intent = Intent(this, MainActivity3::class.java)
-                intent.putExtra("score", score)
-                startActivity(intent)
-                finish()
-            }
-            resultsBtn.setOnClickListener {
-                val intent = Intent(this, MainActivity3::class.java)
-                intent.putExtra("score", score)
-                startActivity(intent)
-                finish()
-            }
+            nextQuestion()
         }
+        resultsBtn.setOnClickListener {
+            showResults()
+        }
+    }
+
+    private fun displayQuestion(){
+        questionDisplay.text = questions[currentQuestionIndex]
+        answerDisplay.text = ""
+    }
+
+    private fun checkAnswer(selectedAnswer: String){
+        if (selectedAnswer == answers[currentQuestionIndex]) {
+            score++
+            answerDisplay.text = "True"
+        } else {
+            answerDisplay.text = "False"
+        }
+        trueBtn.isEnabled = false
+        falseBtn.isEnabled = false
+    }
+    private fun nextQuestion(){
+        currentQuestionIndex++
+        if (currentQuestionIndex < questions.size) {
+            displayQuestion()
+            trueBtn.isEnabled = true
+            falseBtn.isEnabled = true
+        } else {
+            showResults()
+        }
+    }
+
+    private fun showResults(){
+        val intent = Intent(this, MainActivity3::class.java)
+        intent.putExtra("score", score)
+        startActivity(intent)
     }
 }
